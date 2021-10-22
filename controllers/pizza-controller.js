3d2ff121b2beb376aa9011bbf0c57dfe8df68496
Walter Perry <walterjamesperry@gmail.com>
2021-10-21 20:39:59 -0500

    push solution and test Heroku Deployment
    push solution and test Heroku Deployment

---
+++ controllers/pizza-controller.js
@@ -0,0 +1,70 @@
+const { Pizza } = require('../models');
+
+const pizzaController = {
+  getAllPizza(req, res){
+    Pizza.find({})
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
+    .sort({ _id: -1 })
+    .then(dbPizzaData => res.json(dbPizzaData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+  },
+
+  getPizzaById({params}, res){
+    Pizza.findOne({ _id: params.id})
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbPizzaData => {
+        if(!dbPizzaData){
+          res.status(404).json({message: 'No pizza found with this id!'});
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  createPizza({ body }, res){
+    Pizza.create(body)
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.status(400).json(err));
+  },
+
+  updatePizza({params, body}, res){
+    Pizza.findOneAndUpdate({ _id: params.id}, body, { new: true, runValidators: true })
+      .then(dbPizzaData => {
+        if(!dbPizzaData){
+          res.status(404).json({ message: 'No pizza found with this id!'});
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id})
+      .then(dbPizzaData => {
+        if(!dbPizzaData){
+          res.status(404).json({ message: 'No pizza found with this id!'});
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.status(400).json(err));
+  }
+};
+
+module.exports = pizzaController;
---
+++ models/Comment.js
@@ -0,0 +1,59 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReplySchema = new Schema(
+  {
+    replyId:{
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId() 
+    },
+    replyBody:{
+      type: String,
+      required: true,
+      trim: true
+    },
+    writtenBy:{
+      type: String,
+      required: true
+    },
+    createdAt:{
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON:{
+      getters: true
+    }
+  }
+);
+
+const CommentSchema =new Schema(
+  {
+    writtenBy:{
+      type: String,
+      required: true
+    },
+    commentBody:{
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    replies: [ReplySchema]
+  },
+  {
+    toJSON:{
+      getters: true
+    },
+    id: false
+  }
+);
+
+const Comment = model("Comment", CommentSchema);
+
+module.exports = Comment;
